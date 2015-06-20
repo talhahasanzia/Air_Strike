2d01f8dd7edfa56c055fd048e7afa0517f1003d3
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-06-20 15:03:05 +0500

    Fixed Ground Camera
    Implemented ground focusing camera, location and dimensions of camera
    are fixed.

---
+++ AirStrike/Assets/BasicControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicControl : MonoBehaviour {
+	public GameObject AirCraft;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if(Input.GetKeyUp(KeyCode.LeftArrow))
+		   {
+
+			AirCraft.transform.position=new Vector3(AirCraft.transform.position.x,AirCraft.transform.position.y,AirCraft.transform.position.z-10);
+		}
+		if(Input.GetKeyUp(KeyCode.RightArrow))
+
+		   {
+			
+			AirCraft.transform.position=new Vector3(AirCraft.transform.position.x,AirCraft.transform.position.y,AirCraft.transform.position.z+10);
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+		
+			AirCraft.transform.position=new Vector3(AirCraft.transform.position.x-10,AirCraft.transform.position.y,AirCraft.transform.position.z);
+
+		
+		}
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			
+			AirCraft.transform.position=new Vector3(AirCraft.transform.position.x+10,AirCraft.transform.position.y,AirCraft.transform.position.z);
+			
+			
+		}
+	}
+}
