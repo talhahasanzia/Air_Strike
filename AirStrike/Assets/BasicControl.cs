b64fec9bf7918aeb9ccdbfbfa95b2dd5835dba72
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-06-27 15:45:26 +0500

    Fixed Raycast Direction

--- AirStrike/Assets/BasicControl.cs
+++ AirStrike/Assets/BasicControl.cs
@@ -4,20 +4,17 @@ using System.Collections;
 public class BasicControl : MonoBehaviour {
 	public GameObject AirCraft;
 	public GameObject rayCastOrigin;
-	public float AngleX,AngleY,AngleZ;
+	//public float AngleX,AngleY,AngleZ;
 	// Use this for initialization
 	void Start () {
-		AngleX = 0;
-		AngleY = 0;
-		AngleZ = 0;
-	}
+		}
 	
 	// Update is called once per frame
 	void Update ()
     {
 		Vector3 targetDir = new Vector3 (45, 0, 0);
 		RaycastHit info;
-		if (Physics.Raycast (rayCastOrigin.transform.position,Quaternion.Euler(AngleX,AngleY,AngleZ) * transform.forward ,out info,100)) {
+		if (Physics.Raycast (rayCastOrigin.transform.position,Quaternion.Euler(0,0,135) * transform.up ,out info,100)) {
 		
 			Debug.DrawLine(rayCastOrigin.transform.position,info.point);
 			Debug.Log("Raycast working");
