22bef3e48a23eb1be75f45434141918856552055
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-07-06 15:34:14 +0500

    Flak Cloning
    Implemented simple flak cannon shooting randomly

--- AirStrike/Assets/AntiAircraft.cs
+++ AirStrike/Assets/AntiAircraft.cs
@@ -4,30 +4,60 @@ using System.Collections;
 public class AntiAircraft : MonoBehaviour {
 
 	Rigidbody rg_obj;
+    public GameObject StartPosition;
 
 	int randomX,randomZ;
 	// Use this for initialization
 	void Start () {
-		rg_obj = gameObject.GetComponent<Rigidbody> ();	
-		randomX = Random.Range (-120, 120);
-		randomZ = Random.Range (-70, 70);
-		rg_obj.AddForce(randomX,1500,randomZ);
+
+
+       
+        rg_obj = gameObject.GetComponent<Rigidbody>();
+        randomX = Random.Range(-120, 120);
+        randomZ = Random.Range(-70, 70);
+        rg_obj.AddForce(randomX, 1500, randomZ);
+            
+
+        
+        
+            
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		if (rg_obj.velocity == Vector3.zero) {
-		
 
-		
-		
-		}
-	}
+        
+
+        if (gameObject.transform.position.y == StartPosition.transform.position.y)
+        {
+            rg_obj = gameObject.GetComponent<Rigidbody>();
+            rg_obj.velocity = Vector3.zero;
+            randomX = Random.Range(-120, 120);
+            randomZ = Random.Range(-70, 70);
+            rg_obj.AddForce(randomX, 1500, randomZ);
+        }          
+
+
+
+        
+        
+      
+
+
+
+
+			}
 	void OnCollisionEnter(Collision col)
 	{
-		if (col.gameObject.tag == "terrain" || col.gameObject.tag == "plane")
-			Destroy (gameObject);
+        if (col.gameObject.tag == "terrain" || col.gameObject.tag == "plane" || gameObject.transform.position.y>20)
+        {
+            gameObject.transform.position = StartPosition.transform.position;
+            
+            //Destroy(gameObject);
+
+           
+        }
 
 
 	}
